c3fc86993b56d843cae3705f1e58ebf9c31fa94a
CollegeSpace-NSUT <77673387+CollegeSpace-NSUT@users.noreply.github.com>
2021-01-24 22:29:01 +0530

    Update teamData.js

--- src/meta/teamData.js
+++ src/meta/teamData.js
@@ -2,68 +2,156 @@ const teamData = {
   Leadership: [
     {
       positions: ['President'],
-      name: 'Deepika Naryani',
+      name: 'Shrey Jain',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
-    },
-    {
-      positions: ['Founder', 'Web Developer'],
-      name: 'Sahil Gulati',
+    },{
+      positions: ['Director'],
+      name: 'Divya Rawat',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
-    },
-    {
-      positions: ['Founder', 'Web Developer'],
-      name: 'Sahil Gulati',
+    },{
+      positions: ['Director', 'AppD Lead'],
+      name: 'Mohit Yadav',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
-    },
-    {
-      positions: ['Founder', 'Web Developer'],
+    },{
+      positions: ['Tech Lead'],
+      name: 'Anish Jangra',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Tech Lead'],
+      name: 'Shubham',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Management Lead'],
+      name: 'Jigyasu Malhotra',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Creation, Marketing & PR Lead'],
+      name: 'Avanik Gupta',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Founder'],
       name: 'Sahil Gulati',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
     },
   ],
-  WebDevs: [
+  'WebD Team': [
+    {
+      positions: ['WebD Lead'],
+      name: 'Anish Jangra',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['WebD Lead'],
+      name: 'Shubham',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Web Developer'],
+      name: 'Shakib Ahmed',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Web Developer'],
+      name: 'Prakash Ujjawal',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },
+  ],
+  'AppD Team': [
+    {
+      positions: ['Director', 'AppD Lead'],
+      name: 'Mohit Yadav',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['App Developer', 'Content Creator'],
+      name: 'Urja Banati',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },
+  ],
+  'Content Managers': [
+    {
+      positions: ['Content Management Lead'],
+      name: 'Jigyasu Malhotra',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Manager'],
+      name: 'Devesh Garg',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Manager'],
+      name: 'Harshita Yadav',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },
+  ],
+  'Content Writers': [
+    {
+      positions: ['Director', 'Content Creation Lead'],
+      name: 'Divya Rawat',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Creation, Marketing & PR Lead'],
+      name: 'Avanik Gupta',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    },{
+      positions: ['Content Creator', 'App Developer'],
+      name: 'Urja Banati',
+      image: '/img/logo.png',
+      chipImage: '/img/logo.png',
+      bio: '',
+    }
+  ],
+  Designers: [
     {
       positions: ['President'],
-      name: 'Deepika Naryani',
+      name: 'Shrey Jain',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
     },
+  ],
+  Alumni: [
     {
-      positions: ['Founder', 'Web Developer'],
+      positions: ['Founder'],
       name: 'Sahil Gulati',
       image: '/img/logo.png',
       chipImage: '/img/logo.png',
       bio: '',
     },
   ],
-  'Content Collectors': [
-    {
-      positions: ['President'],
-      name: 'Deepika Naryani',
-      image: '/img/logo.png',
-      chipImage: '/img/logo.png',
-      bio: '',
-    },
-    {
-      positions: ['Founder', 'Web Developer'],
-      name: 'Sahil Gulati',
-      image: '/img/logo.png',
-      chipImage: '/img/logo.png',
-      bio: '',
-    },
-  ],
-  'Content Writers': [],
-  Designers: [],
-  Alumni: [],
 };
 
 export default teamData;
