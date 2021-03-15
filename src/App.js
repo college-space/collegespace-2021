893544713ea153d820fc82fa2f05bf3b5d87e38a
shakib1729 <shakibahmed0786@gmail.com>
2021-03-15 21:13:14 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Footer />
     </div>
   );
 }
