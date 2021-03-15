4fb538908c639356e276231ae75208d67c82ac60
shakib1729 <shakibahmed0786@gmail.com>
2021-03-15 21:20:12 +0530

    added sidenav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
+import Sidenav from './components/Layout/Sidenav';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Sidenav />
+        <main className='container'>Main</main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Layout/Sidenav.js
@@ -0,0 +1,122 @@
+import React, { Component, useRef, useEffect } from 'react';
+import M from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import { Icon, Collapsible } from 'react-materialize';
+import { NavLink } from 'react-router-dom';
+import { getImageURL } from '../../utils/getImageURL';
+
+const Sidenav = () => {
+  const sidenavRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      inDuration: 50,
+      outDuration: 20,
+      draggable: true,
+    };
+
+    M.Sidenav.init(sidenavRef.current);
+
+    let instance = M.Sidenav.getInstance(sidenavRef.current);
+    // instance.open();
+  }, []);
+  const branchList = ['coe', 'it', 'ece'];
+  return (
+    <>
+      <ul ref={sidenavRef} id='slide-out' className='sidenav sidenav-fixed'>
+        <li>
+          <img
+            className='container'
+            src={getImageURL(
+              'https://res.cloudinary.com/collegespace/image/upload/v1614059741/Logos/logo_dwvpxw.png',
+              300,
+              -1,
+              -1,
+              -1
+            )}
+          />
+        </li>
+
+        <li>
+          <NavLink to={'/'} className='waves-effect'>
+            <i className='material-icons'>cloud</i>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={'/team'} className='waves-effect'>
+            <i className='material-icons'>color_lens</i>
+            Meet Our Team
+          </NavLink>
+        </li>
+
+        <li className='no-padding'>
+          <Collapsible accordion={true} className='collapsible-accordion'>
+            <li className='bold'>
+              <a className='collapsible-header waves-effect waves-teal'>
+                Examspace
+                <Icon>keyboard_arrow_down</Icon>
+              </a>
+              <div className='collapsible-body'>
+                <ul>
+                  {branchList.map((branch, index) => (
+                    <li>
+                      <NavLink
+                        to={`/examspace/${branch}`}
+                        className='waves-effect'
+                      >
+                        <i className='material-icons'>all_inclusive</i>
+                        {branch.toUpperCase()}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </Collapsible>
+        </li>
+
+        <li className='no-padding'>
+          <Collapsible accordion={true} className='collapsible-accordion'>
+            <li className='bold'>
+              <a className='collapsible-header waves-effect waves-teal'>
+                LearnSpace
+                <Icon>keyboard_arrow_down</Icon>
+              </a>
+              <div className='collapsible-body'>
+                <ul>
+                  <li>
+                    <NavLink
+                      to={'/learnspace/internships'}
+                      className='waves-effect'
+                    >
+                      <i className='material-icons'>toys</i>
+                      Internships
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={'/learnspace/placements'}
+                      className='waves-effect'
+                    >
+                      <i className='material-icons'>toys</i>
+                      Placements
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </Collapsible>
+        </li>
+      </ul>
+      <a
+        href='#!'
+        data-target='slide-out'
+        className='sidenav-trigger hide-on-large-only'
+      >
+        <i className='material-icons'>menu</i>
+      </a>
+    </>
+  );
+};
+
+export default Sidenav;
